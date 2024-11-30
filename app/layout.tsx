69d6690c2c494eddb5fcc9ddceba39fec352e44a
Maxzyto <adaaziabugrimaxwell@gmail.com>
2024-11-30 08:25:42 +0000

    styling the ScrollTop

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
+import Footer from '@/components/Home/Footer/Footer';
+import ScrollTop from '@/components/Helper/ScrollTop';
+import AnimatedCursor from "react-animated-cursor";
+
 
 const font = Sora({ 
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -19,12 +23,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}
-      >
+    <html lang="en" className='custom-scrollbar'>
+      <body className={font.className}>
+        <div className="hidden md:block">
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={35}
+            innerScale={2}
+            outerScale={2}
+            outerAlpha={0}
+            innerStyle={{
+              backgroundColor: "white",
+            }}
+            outerStyle={
+              {
+                border:"3px solid white",
+              }
+            }
+          />
+         
+        </div>
         <ResponsiveNav />
         {children}
-        
+        <Footer />
+        <ScrollTop />
       </body>
     </html>
   );
--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <ContactForm />
         </div>
         {/* C. info  */}
-        <div>
+        <div className='xl:mx-auto'>
           <ContactInfo />
         </div>
       </div>
